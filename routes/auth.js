c542932449c0ed7f48c2adcda893efce737347fb
iNKidzy <nadq.nadia@gmail.com>
2023-02-18 20:31:00 +0100

    signup

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const router  = require("express").Router();
 const user = require("../models/user");
 const { signupValidation } = require("../validation");
+const bcrypt = require('bcrypt');
 
 
 //Routes for registration, login 
@@ -24,13 +25,26 @@ router.post("/signup", async(req,res) => {
     }
 
     //hash password 
-    //const salt = await 
-    
+    const salt = await bcrypt.genSalt(10); //generate Salt using bcrypt, to be applied: x rounds.
+    const password = await bcrypt.hash(req.body.password, salt); //generate a hash based on the pass we get from req + the salt
+
     //create a user object and save in db
+    const newUser = new user({
+        username: req.body.username,
+        name: req.body.name,
+        email: req.body.email,
+        password
+    });
 
+    try {
+        const savedUser = await newUser.save();
+        res.json({ error: null, data: savedUser._id });
+    
+    } catch (error) {
+        res.status(400).json({ error });
+    }
 
-
-    return res.status(200).json({msg: "User registered successfully!!!"});
+    // return res.status(200).json({msg: "User registered successfully!!!"});
 
 });
 
