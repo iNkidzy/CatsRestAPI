9999db64507d685c9cd9757f4ab28b1de95a18ed
iNKidzy <nadq.nadia@gmail.com>
2023-02-18 20:16:40 +0100

    validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const router  = require("express").Router();
 const user = require("../models/user");
+const { signupValidation } = require("../validation");
 
 
 //Routes for registration, login 
@@ -9,22 +10,33 @@ router.post("/signup", async(req,res) => {
     //code
    
     //validate user input (username,email,password,name?)
+    const {error} = signupValidation(req.body);
+    
+    if(error) {
+        return res.status(400).json({error: error.details[0].message });
+    }
 
     //check if email already exists
+    const duplicateEmail = await user.findOne({email: req.body.email});
+
+    if(duplicateEmail){
+        return res.status(400).json({error: "Error: Email already exists!" });
+    }
 
     //hash password 
-
+    //const salt = await 
+    
     //create a user object and save in db
 
 
 
-    return res.status(200).json({msg: "Register route"});
+    return res.status(200).json({msg: "User registered successfully!!!"});
 
 });
 
 router.post("/login", async(req,res) => {
     //
-    return res.status(200).json({msg: "Login route"});
+    return res.status(200).json({msg: "Success!! Login success!!"});
 });
 
 module.exports = router;
---
+++ validation.js
@@ -0,0 +1,20 @@
+const Joi = require('joi');
+
+
+// Validating registration
+const signupValidation = (data) => {
+    const schema = Joi.object(
+        {
+            username: Joi.string().min(4).max(10).required(),
+            name: Joi.string().min(4).max(50),
+            email: Joi.string().min(6).max(255).required(),
+            password: Joi.string().min(8).max(255).required(),
+        });
+    return schema.validate(data);
+}
+
+// Validating login
+
+//verify token (JWT)
+
+module.exports = { signupValidation };
